7deb7c9f2270062fdbd1e2414414d3c7b7fae527
matheus-monteiro <mmonteiro100@yahoo.com>
2017-12-09 13:43:19 -0300

    31_11_2018

---
+++ 2018/31_11/H.cpp
@@ -0,0 +1,29 @@
+// Author : Matheus Monteiro
+// Link: https://www.urionlinejudge.com.br/repository/UOJ_1661.html
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+
+ll n;
+
+int main()
+{
+	while(cin >> n and n)
+	{
+		ll ans = 0;
+		ll m, k = 0;
+		while(n--)
+		{
+			cin >> m;
+			k += m;
+			ans += abs(k);
+		}
+		cout << ans << '\n';
+	}
+	
+	return 0;	
+}
